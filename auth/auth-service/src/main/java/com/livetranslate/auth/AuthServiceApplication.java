c2267ba051a877699464f85f322a7995ce80ff9f
HanDon001 <handon99780@gmail.com>
2026-06-05 23:29:26 +0800

    修复构建：添加启动类和配置文件

---
+++ auth/auth-service/src/main/java/com/livetranslate/auth/AuthServiceApplication.java
@@ -0,0 +1,15 @@
+package com.livetranslate.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 认证服务启动类
+ */
+@SpringBootApplication
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
---
+++ services/asr-service/asr-server/src/main/java/com/livetranslate/asr/ASRServiceApplication.java
@@ -0,0 +1,15 @@
+package com.livetranslate.asr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * ASR 服务启动类
+ */
+@SpringBootApplication
+public class ASRServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ASRServiceApplication.class, args);
+    }
+}
